c7ecdefc31949a8f0a8affc9f9a852244c0634be
nmondon <nmondon@lefigaro.fr>
2015-02-18 13:05:51 +0100

    - Webpack : comment config file

--- webpack.config.js
+++ webpack.config.js
@@ -4,22 +4,27 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
+    // our different entries
     entry: {
         'background': './web_modules/background.js',
         'options': './web_modules/options.js',
         'content': './web_modules/content.js',
         'popup': './web_modules/popup.jsx'
     },
+    // output configuration
     output: {
         path: './chrome_extension/js',
         filename: '[name].js'
     },
     resolve: {
+        // modules dir
         root: [
             path.resolve('node_modules'),
             path.resolve('web_modules')
         ],
+        // allowed extensions
         extensions: ['', '.js', '.jsx'],
+        // alias for vendors
         alias: {
             jquery: path.join(__dirname, 'node_modules/jquery/dist/jquery.js'),
             react: path.join(__dirname, 'node_modules/react/addons.js'),
@@ -27,6 +32,7 @@ module.exports = {
         }
     },
     module: {
+        // specific loaders
         loaders: [
             // jsx
             { test: /\.jsx$/, loader: 'jsx-loader'}
